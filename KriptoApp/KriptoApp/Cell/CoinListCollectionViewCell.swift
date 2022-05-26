f26c9628817e2781022002e4ead8426175f605bb
yusuf <yusufyldrm1625@icolud.com>
2022-05-26 18:15:19 +0300

    add project

---
+++ KriptoApp/KriptoApp/Cell/CoinListCollectionViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  CoinListCollectionViewCell.swift
+//  KriptoApp
+//
+//  Created by Yusuf Muhammet Yıldırım on 5/25/22.
+//
+import UIKit
+
+class CoinListCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var coinImage: UIImageView!
+    @IBOutlet weak var symbolLabel: UILabel!
+    @IBOutlet weak var coinName: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var changeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    func configure(model : CoinElement){
+        let newUrl = model.iconUrl.replacingOccurrences(of: "svg", with: "png")
+        let url = URL(string: (newUrl))
+                let data = try? Data(contentsOf: url!)
+                if let imageData = data {
+                    self.coinImage.image = UIImage(data: imageData)
+                }
+        coinName.text = model.name
+        symbolLabel.text = model.symbol
+        changeLabel.text = "\(model.change)%"
+        priceLabel.text = String(format: "$%.2f", Double(model.price)!)
+
+    }
+
+}
---
+++ KriptoApp/KriptoApp/Model/CoinDataModel.swift
@@ -0,0 +1,38 @@
+//
+//  CoinDataModel.swift
+//  KriptoApp
+//
+//  Created by Yusuf Muhammet Yıldırım on 5/25/22.
+//
+
+
+import Foundation
+
+struct CoinDataModel: Codable {
+    let data : DataClass
+}
+
+struct DataClass: Codable {
+    let coins : [CoinElement]
+}
+
+struct CoinElement: Codable {
+    let uuid : String
+    let symbol : String
+    let name: String
+    let iconUrl: String
+    let marketCap : String
+    let price: String
+    let listedAt : Int
+    let change: String
+    let rank: Int
+    let the24HVolume : String
+    let sparkline: [String]
+    
+    enum CodingKeys : String, CodingKey {
+        case uuid,symbol,name,iconUrl,marketCap,price,listedAt,change,rank,sparkline
+        case the24HVolume = "24hVolume"
+        
+    }
+
+}
